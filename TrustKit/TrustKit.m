7a0134ed387c0dfab651784fb2911bb0b480d638
Alban Diquet <nabla.c0d3@gmail.com>
2015-02-10 12:21:43 -0800

    Switch to public key pinning

--- TrustKit/TrustKit.m
+++ TrustKit/TrustKit.m
@@ -12,10 +12,58 @@
 #import "fishhook/fishhook.h"
 
 
+// Info.plist key we read the public key hashes from
 static const NSString *TrustKitInfoDictionnaryKey = @"TSKCertificatePins";
 
 
+// Global storing the public key hashes and domains
 static NSMutableDictionary *certificatePins = NULL;
+
+
+// The ASN1 data for a public key returned by iOS lacks the following ASN1 header
+unsigned char missingRsaAsn1Header[] = {
+    0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
+    0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0f, 0x00
+};
+
+
+
+#pragma mark Public Key Converter
+
+static const NSString *TrustKitPublicKeyTag = @"TSKPublicKeyTag"; // Used to add and find the public key in the Keychain
+
+// The one and only way to get a key's data in a buffer on iOS is to put it in the Keychain and then ask for the data back...
+static NSData *getPublicKeyBits(SecKeyRef publicKey)
+{
+    NSData *publicKeyData = nil;
+    OSStatus resultAdd, resultGet, resultDel = noErr;
+    
+    
+    // Prepare the dictionnary to add the key
+    NSMutableDictionary *peerPublicKeyAdd = [[NSMutableDictionary alloc] init];
+    [peerPublicKeyAdd setObject:(__bridge id)kSecClassKey forKey:(__bridge id)kSecClass];
+    [peerPublicKeyAdd setObject:TrustKitPublicKeyTag forKey:(__bridge id)kSecAttrApplicationTag];
+    [peerPublicKeyAdd setObject:(__bridge id)(publicKey) forKey:(__bridge id)kSecValueRef];
+    
+    // Prepare the dictionnary to retrieve the key
+    NSMutableDictionary * publicKeyGet = [[NSMutableDictionary alloc] init];
+    [publicKeyGet setObject:(__bridge id)kSecClassKey forKey:(__bridge id)kSecClass];
+    [publicKeyGet setObject:(TrustKitPublicKeyTag) forKey:(__bridge id)kSecAttrApplicationTag];
+    [publicKeyGet setObject:[NSNumber numberWithBool:YES] forKey:(__bridge id)kSecReturnData];
+    
+    
+    // Get the key bytes from the Keychain atomically
+    @synchronized(certificatePins)
+    {
+        resultAdd = SecItemAdd((__bridge CFDictionaryRef) peerPublicKeyAdd, NULL);
+        resultGet = SecItemCopyMatching((__bridge CFDictionaryRef)publicKeyGet, (void *)&publicKeyData);
+        resultDel = SecItemDelete((__bridge CFDictionaryRef)(publicKeyGet));
+    }
+    
+    //NSLog(@"RESULT %d", result);
+    
+    return publicKeyData;
+}
 
 
 #pragma mark Certificate Pin Validator
@@ -44,7 +92,7 @@ static BOOL verifyCertificatePin(SecTrustRef serverTrust, NSString *serverName)
     
     // For each pinned certificate, check if it is part of the server's cert trust chain
     // We only need one of the pinned certificates to be in the server's trust chain
-    for (NSData *pinnedCertificateHash in serverPins)
+    for (NSData *pinnedSubkectPublicKeyInfoHash in serverPins)
     {
         // Check each certificate in the server's certificate chain (the trust object)
         CFIndex certificateChainLen = SecTrustGetCertificateCount(serverTrust);
@@ -52,15 +100,29 @@ static BOOL verifyCertificatePin(SecTrustRef serverTrust, NSString *serverName)
             
             // Extract the certificate
             SecCertificateRef certificate = SecTrustGetCertificateAtIndex(serverTrust, i);
-            NSData* certificateAsDer = (__bridge NSData*) SecCertificateCopyData(certificate);
+    
+            // Extract the public key
+            SecTrustRef tempTrust;
+            SecPolicyRef policy = SecPolicyCreateBasicX509();
+            SecTrustCreateWithCertificates(certificate, policy, &tempTrust);
+            SecTrustEvaluate(tempTrust, NULL);
+            SecKeyRef publicKey = SecTrustCopyPublicKey(tempTrust);
+            NSData *publicKeyData = getPublicKeyBits(publicKey);
             
-            // Hash the certificate
-            NSMutableData *certificateHash = [NSMutableData dataWithLength:CC_SHA256_DIGEST_LENGTH];
-            CC_SHA256(certificateAsDer.bytes, (unsigned int)certificateAsDer.length,  certificateHash.mutableBytes);
-            NSLog(@"PinE %@  PinF %@", pinnedCertificateHash, certificateHash);
+            // Add the missing ASN1 header for RSA public keys to re-create the subject public key info
+            NSMutableData *subjectPublicKeyInfoData = [NSMutableData dataWithBytes:missingRsaAsn1Header length:sizeof(missingRsaAsn1Header)];
+            [subjectPublicKeyInfoData appendData:publicKeyData];
+            //NSLog(@"%@ SUBJECT KEY DATA %@", serverName, subjectPublicKeyInfoData);
+            
+            
+            // Hash the public key
+            NSMutableData *subjectPublicKeyInfoHash = [NSMutableData dataWithLength:CC_SHA256_DIGEST_LENGTH];
+            CC_SHA256(subjectPublicKeyInfoData.bytes, (unsigned int)subjectPublicKeyInfoData.length,  subjectPublicKeyInfoHash.mutableBytes);
+            CFRelease(publicKey);
+            NSLog(@"PinE %@  PinF %@", pinnedSubkectPublicKeyInfoHash, subjectPublicKeyInfoHash);
             
             // Compare the two hashes
-            if ([pinnedCertificateHash isEqualToData:certificateHash])
+            if ([pinnedSubkectPublicKeyInfoHash isEqualToData:subjectPublicKeyInfoHash])
             {
                 NSLog(@"OK: Found SSL Pin");
                 return YES;
@@ -69,10 +131,10 @@ static BOOL verifyCertificatePin(SecTrustRef serverTrust, NSString *serverName)
     }
  
     // If we get here, we didn't find any matching certificate in the chain
-    
     NSLog(@"Error: SSL Pin not found");
     return NO;
 }
+
 
 #pragma mark SSLHandshake Hook
 static OSStatus (*original_SSLHandshake)(SSLContextRef context);
@@ -135,7 +197,7 @@ static OSStatus replaced_SSLHandshake(SSLContextRef context)
 
 
 
-#pragma mark Framework Constructor
+#pragma mark Framework Initialization
 
 __attribute__((constructor)) static void initialize(int argc, const char **argv)
 {
@@ -145,7 +207,7 @@ __attribute__((constructor)) static void initialize(int argc, const char **argv)
     certificatePins = [[NSMutableDictionary alloc]init];
     
         // Retrieve the certificate hashes/pins from the App's Info.plist file
-     NSDictionary *certificatePinsFromPlist = [[NSBundle mainBundle] objectForInfoDictionaryKey:TrustKitInfoDictionnaryKey];
+     NSDictionary *certificatePinsFromPlist = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)TrustKitInfoDictionnaryKey];
     
     
     // Convert the certificates hashes/pins to NSData and store them in the certificatePins variable
