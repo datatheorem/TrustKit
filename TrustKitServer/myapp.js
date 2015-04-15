0dc8eaba6d5844e78c396a60777a7897d387f24d
Angela Chow <paranoid_angela@yahoo.com>
2015-04-15 02:13:06 -0700

    add files to run server

---
+++ TrustKitServer/myapp.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); //for parsing multipart/form-data
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send('got a POST request');
+//  res.json(req.body);
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
