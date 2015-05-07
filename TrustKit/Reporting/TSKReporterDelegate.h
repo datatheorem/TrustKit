1df294c423fa8cd62a1878fb52cd1e47b3208c96
Angela Chow <paranoid_angela@yahoo.com>
2015-05-07 11:13:26 -0700

    add simple reporter

--- TrustKit/Reporting/TSKReporterDelegate.h
+++ TrustKit/Reporting/TSKReporterDelegate.h
@@ -10,26 +10,34 @@
 
 @protocol TSKReporterDelegate <NSObject>
 /*
- * Initialize the reporter with the app's bundle id, app version, and the URL to send the reports to
+ * Initialize the reporter with the app's bundle id, and app version
  */
-- (instancetype)initWithAppBundleId:(NSString *) appBundleId
-                         appVersion:(NSString *) appVersion
-                       reportingURL:(NSString *) reportingURL;
+- (void)initWithAppBundleId:(NSString *) appBundleId
+                 appVersion:(NSString *) appVersion;
 
-@optional
 
 /*
  * Pin validation failed for a connection to a pinned domain
  */
-- (void) pinValidationFailed:(NSString *) pinnedDomain
-              serverHostname:(NSString *) hostname
-                  serverPort:(NSString *) port
+- (void) pinValidationFailed:(NSString *) pinnedDomainStr
+              serverHostname:(NSString *) hostnameStr
+                  serverPort:(NSNumber *) port
+                reportingURL:(NSString *) reportingURLStr
            includeSubdomains:(Boolean) includeSubdomains
             certificateChain:(NSArray *) validatedCertificateChain
                 expectedPins:(NSArray *) knownPins;
 
+@optional
+
 /*
- * Pin validation succeeded for a connection to a pinned domain
+ * Pin validation succeeded for a connection to a pinned domain.
+ *
+ * This is an optional function, as usually, reports are only sent upon pin failure.  However, a
+ * smart reporter would realize that sending reports while connections are being MITM is not the
+ * best way of ensuring reports are being successfully received.  Therefore, those reporters
+ * may delay the sending of those reports and preferably send them when the connections
+ * are no longer in the MITM state.  In order to know that connections are no longer MITM, 
+ * the pinValidationSucceeded function will provide a way to know that connections are fine now.
  */
 - (void) pinValidationSucceeded:(NSString*) pinnedDomain;
 
