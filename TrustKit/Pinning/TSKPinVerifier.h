0d74f1ba9594c02eb12047e150de9fbf773780c4
Alban Diquet <nabla.c0d3@gmail.com>
2015-06-01 11:32:01 +0200

    Add documentation

--- TrustKit/Pinning/TSKPinVerifier.h
+++ TrustKit/Pinning/TSKPinVerifier.h
@@ -8,18 +8,60 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Possible return values when verifying a server's identity against the global SSL pinning policy using `TSKPinVerifier`.
 
-// Pin validation result values
-typedef NS_ENUM(NSInteger, TSKPinValidationResult) {
+ */
+typedef NS_ENUM(NSInteger, TSKPinValidationResult)
+{
+  /**
+   The server trust was succesfully evaluated and contained at least one of the configured pins.
+  */
     TSKPinValidationResultSuccess,
+    
+ /**
+  The server trust was succesfully evaluated but did not contain any of the configured pins.
+ */
     TSKPinValidationResultFailed,
-    TSKPinValidationResultFailedInvalidCertificateChain, // The server's supplied certificate chain is not trusted
+    
+ /**
+  The server trust's evaluation failed: the server's certificate chain is not trusted.
+  */
+    TSKPinValidationResultFailedInvalidCertificateChain,
+    
+ /**
+  The server trust could not be evaluated due to invalid parameters.
+ */
     TSKPinValidationResultFailedInvalidParameters,
 };
 
 
+/**
+ `TSKPinVerifier` is a class for manually verifying a server's identity against the global SSL pinning policy.
+ 
+ This method should only be used to validate certificate chains for SSL connections that are not automatically handled by TrustKit:
+ 
+ * Background upload/download URL sessions, because they are performed in a different process where TrustKit doesn't get loaded.
+ * Connections that don't rely on Apple's SecureTransport for SSL and use a third-party library instead (such as OpenSSL).
+ 
+ */
 @interface TSKPinVerifier : NSObject
 
+///--------------------------------
+/// @name Manual SSL Pin Validation
+///--------------------------------
+
+/**
+ Verify the validity of the supplied server trust against the global SSL pinning policy previously configured.
+ 
+ @param serverTrust The trust object representing the server's certificate chain. The trust's validation policy is always overriden to ensure all the proper SSL policies (expiration, hostname validation, etc.) are enabled.
+ 
+ @param serverHostname The hostname of the server whose identity is being validated.
+ 
+ @return The result of validation. See `TSKPinValidationResult` for possible values.
+ 
+ @warning If no SSL pinning policy was configured for the supplied _serverHostname_, this method has no effect and will return `TSKPinValidationResultSuccess` without validating the supplied _serverTrust_ at all.
+ */
 + (TSKPinValidationResult) verifyPinForTrust:(SecTrustRef)serverTrust andHostname:(NSString *)serverHostname;
 
 @end
--- TrustKit/TrustKit.h
+++ TrustKit/TrustKit.h
@@ -29,7 +29,7 @@ extern NSString * const kTSKAlgorithmEcDsaSecp256r1;
 
 
 /**
- `TrustKit` is a utility class for configuring the SSL pinning policy in an App that statically links TrustKit.
+ `TrustKit` is a class for configuring the global SSL pinning policy in an App that statically links TrustKit.
  
  Initializing TrustKit requires supplying a dictionary containing domain names as keys and dictionaries as values. Each domain dictionary should specify some configuration keys, which will specify the pinning policy for this domain. For example:
  
@@ -41,19 +41,28 @@ extern NSString * const kTSKAlgorithmEcDsaSecp256r1;
                  @"0SDf3cRToyZJaMsoS17oF72VMavLxj/N7WBNasNuiR8="
                  ]
              }
+        @"yahoo.com" : @{
+            kTSKPublicKeyAlgorithms : @[kTSKAlgorithmRsa4096],
+            kTSKPublicKeyHashes : @[
+                @"TQEtdMbmwFgYUifM4LDF+xgEtd0z69mPGmkp014d6ZY=",
+                @"rFjc3wG7lTZe43zeYTvPq8k4xdDEutCmIhI5dn4oCeE=",
+                ]
+            },
+            kTSKIncludeSubdomains : YES
          };
+ 
      [TrustKit initializeWithConfiguration:trustKitConfig];
  
-  When dynamically linking TrustKit, the Framework is automatically initialized by reading configuration keys from the App's Info.plist, and no initialization method needs to be called.
+  When dynamically linked, TrustKit is automatically initialized by reading configuration keys from the App's _Info.plist_, and no initialization method needs to be called.
  
  
  ### Required Configuration Keys
  
  #### `kTSKPublicKeyHashes`
- This value should be an array of SSL pins; each pin is the base64-encoded SHA 256 of a certificate's subject public key info. TrustKit will verify that at least one of the specifed pins is found in the server's certificate chain.
+ This value should be an array of SSL pins; each pin is the base64-encoded SHA-256 hash of a certificate's subject public key info. TrustKit will verify that at least one of the specifed pins is found in the server's certificate chain.
  
  #### `kTSKPublicKeyAlgorithms`
- This value should be an array of algorithm constants (kTSKAlgorithm) that TrustKit needs to support when generating public key hashes. Supporting multiple algorithms has a performance impact.
+ This value should be an array of `kTSKAlgorithm` constants that TrustKit needs to support when computing pins. To minimize the performance impact of Trustkit, only one algorithm should be enabled.
  
  
  ### Optional Configuration Keys
@@ -62,10 +71,10 @@ extern NSString * const kTSKAlgorithmEcDsaSecp256r1;
  If set to `YES`, also pin all the subdomains of the specified domain; default value is `NO`.
  
  #### `kTSKEnforcePinning`
- If set to `NO`, a pinning failure will not cause the connection to fail; default value is `YES`.
+ If set to `NO`, a pinning failure will not cause the SSL connection to fail; default value is `YES`.
  
  #### `kTSKReportUris`
- No effect at the moment.
+ An array of URLs to which pin validation failures should be reported. The format of the reports is similar to the one described in the HPKP specification.
  
  
  ### Public Key Algorithms Keys
@@ -86,11 +95,11 @@ extern NSString * const kTSKAlgorithmEcDsaSecp256r1;
 ///---------------------
 
 /**
- Initializes TrustKit's global state with the supplied SSL pinning configuration. 
+ Initializes the global SSL pinning policy with the supplied configuration.
  
  This method should be called as early as possible in the App's lifecycle to ensure that the App's very first HTTPS connections are protected.
  
- @param trustKitConfig A dictionnary containing various keys for configuring the App's SSL pinning policy.
+ @param trustKitConfig A dictionnary containing various keys for configuring the global SSL pinning policy.
  
  @warning TrustKit can only be initialized once and calling this method multiple times will raise an exception.
  */
