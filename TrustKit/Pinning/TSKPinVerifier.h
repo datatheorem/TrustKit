0a73bd6187fc1f4cd4be250ada0db1d2d7f7da23
Alban Diquet <nabla.c0d3@gmail.com>
2015-07-10 13:48:54 -0700

    Fix documentation formatting

--- TrustKit/Pinning/TSKPinVerifier.h
+++ TrustKit/Pinning/TSKPinVerifier.h
@@ -17,34 +17,34 @@
  */
 typedef NS_ENUM(NSInteger, TSKPinValidationResult)
 {
-    /**
-     The server trust was succesfully evaluated and contained at least one of the configured pins.
-     */
+/**
+ The server trust was succesfully evaluated and contained at least one of the configured pins.
+*/
     TSKPinValidationResultSuccess,
     
-    /**
-     The server trust was succesfully evaluated but did not contain any of the configured pins.
-     */
+/**
+ The server trust was succesfully evaluated but did not contain any of the configured pins.
+*/
     TSKPinValidationResultFailed,
     
-    /**
-     The server trust's evaluation failed: the server's certificate chain is not trusted.
-     */
+/**
+ The server trust's evaluation failed: the server's certificate chain is not trusted.
+*/
     TSKPinValidationResultFailedCertificateChainNotTrusted,
     
-    /**
-     The server trust could not be evaluated due to invalid parameters.
-     */
+/**
+ The server trust could not be evaluated due to invalid parameters.
+*/
     TSKPinValidationResultErrorInvalidParameters,
     
-    /**
-     The supplied hostname does not have a pinning policy configured; no validation was performed.
-     */
+/**
+ The supplied hostname does not have a pinning policy configured; no validation was performed.
+*/
     TSKPinValidationResultDomainNotPinned,
     
-    /**
-     The server trust was succesfully evaluated but did not contain any of the configured pins. However, the certificate chain terminates at a user-defined trust anchor (ie. a custom/private CA that was manually added to OS X's trust store).
-     */
+/**
+ The server trust was succesfully evaluated but did not contain any of the configured pins. However, the certificate chain terminates at a user-defined trust anchor (ie. a custom/private CA that was manually added to OS X's trust store). Only available on OS X.
+*/
     TSKPinValidationResultFailedUserDefinedTrustAnchor NS_AVAILABLE_MAC(10_9),
 };
 
@@ -56,10 +56,10 @@ typedef NS_ENUM(NSInteger, TSKPinValidationResult)
  
  The following scenarios require manual pin validation:
  
- * Connections initiated from an external process, where TrustKit does not get loaded:
- * `WKWebView` connections: the server's trust object can be retrieved within the `webView:didReceiveAuthenticationChallenge:completionHandler:` method.
- * `NSURLSession` connections using the background transfer service: the server's trust object can be retrieved within the `application:handleEventsForBackgroundURLSession:completionHandler:` method.
- * Connections initiated using a third-party SSL library such as OpenSSL, instead of Apple's SecureTransport. The server's trust object needs to be built using its certificate chain.
+ 1. Connections initiated from an external process, where TrustKit does not get loaded:
+     * `WKWebView` connections: the server's trust object can be retrieved within the `webView:didReceiveAuthenticationChallenge:completionHandler:` method.
+     * `NSURLSession` connections using the background transfer service: the server's trust object can be retrieved within the `application:handleEventsForBackgroundURLSession:completionHandler:` method.
+ 2. Connections initiated using a third-party SSL library such as OpenSSL, instead of Apple's SecureTransport. The server's trust object needs to be built using the received certificate chain.
  
  */
 @interface TSKPinVerifier : NSObject
@@ -69,13 +69,13 @@ typedef NS_ENUM(NSInteger, TSKPinValidationResult)
 ///--------------------------------
 
 /**
- Verify the validity of the supplied server trust against the global SSL pinning policy previously configured; if pin validation fails, pin failure reports will be sent according to the to the pinning policy.
+ Verify the validity of the supplied server trust against the global SSL pinning policy previously configured; if pin validation fails, pin failure reports will be sent according to the pinning policy.
  
  @param serverTrust The trust object representing the server's certificate chain. The trust's validation policy is always overriden to ensure all the proper SSL policies (expiration, hostname validation, etc.) are enabled.
  
  @param serverHostname The hostname of the server whose identity is being validated.
  
- @return The result of validation. See `TSKPinValidationResult` for possible values.
+ @return The result of the validation. See `TSKPinValidationResult` for possible values.
  
  @warning If no SSL pinning policy was configured for the supplied _serverHostname_, this method has no effect and will return `TSKPinValidationResultDomainNotPinned` without validating the supplied _serverTrust_ at all.
  */
