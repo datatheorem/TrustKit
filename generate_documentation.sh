702c681e5ad9ab69be80393a950367eff186008f
Alban Diquet <ad@sourcetheorem.com>
2017-06-22 17:41:44 -0700

    Bump version number in the documentation

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -6,8 +6,8 @@ jazzy \
   --author "Data Theorem" \
   --author_url https://datatheorem.github.io \
   --github_url https://github.com/datatheorem/TrustKit \
-  --github-file-prefix https://github.com/datatheorem/TrustKit/tree/1.4.2 \
-  --module-version 1.4.2 \
+  --github-file-prefix https://github.com/datatheorem/TrustKit/tree/1.5.0 \
+  --module-version 1.5.0 \
   --umbrella-header TrustKit/TrustKit.h \
   --framework-root . \
   --module TrustKit \
