710cebab9e85651fb47c31f39339f57346ae54c3
Alban Diquet <ad@sourcetheorem.com>
2017-06-23 12:54:13 -0700

    Clarify documentation

--- TrustKit/TSKPinningValidatorResult.h
+++ TrustKit/TSKPinningValidatorResult.h
@@ -20,51 +20,49 @@
 @property (nonatomic, readonly, nonnull) NSString *serverHostname;
 
 /**
- The original SecTrustRef that validation was performed against.
+ The original `SecTrustRef` that validation was performed against.
  */
 @property (nonatomic, readonly, nonnull) SecTrustRef serverTrust;
 
 /**
  The entry within the SSL pinning configuration that was used as the pinning policy for the
- server being validated. It will be the same as the `kTSKValidationServerHostnameNotificationKey` 
- entry unless the server is a subdomain of a domain configured in the pinning policy with
- `kTSKIncludeSubdomains` enabled. The corresponding pinning configuration that was used
- for validation can be retrieved using:
+ server being validated. It will be the same as the `serverHostname` unless the server is a 
+ subdomain of the domain configured in the pinning policy with `kTSKIncludeSubdomains` enabled. 
+ The corresponding pinning configuration that was used for validation can be retrieved using:
 
-     NSString *notedHostname = userInfo[kTSKValidationNotedHostnameNotificationKey];
-     NSDictionary *hostnameConfiguration = [TrustKit configuration][kTSKPinnedDomains][notedHostname];
+     NSDictionary *hostnameConfiguration = [trustKit configuration][kTSKPinnedDomains][notedHostname];
  */
 @property (nonatomic, readonly, nonnull) NSString *notedHostname;
 
 /**
- The `TSKPinValidationResult` returned when validating the server's certificate chain, which
- represents the result of evaluating the certificate chain against the configured SSL pins 
- for this server.
+ The result of validating the server's certificate chain against the set of SSL pins configured for 
+ the `notedHostname`.
  */
 @property (nonatomic, readonly) TSKTrustEvaluationResult validationResult;
 
 /**
- The `TSKTrustDecision` returned when validating the certificate's chain, which describes
- whether the connection should be blocked or allowed, based on the `TSKPinningValidationResult`
- returned when evaluating the server's certificate chain and the SSL pining policy configured 
- for this server.
+ The trust decision returned for this connection, which describes whether the connection should be blocked 
+ or allowed, based on the `validationResult` returned when evaluating the `serverTrust` and the SSL pining 
+ policy configured for this server.
 
- For example, the pinning validation could have failed (returning `TSKPinningValidationFailed`)
- but the policy might be set to ignore pinning validation failures for this server, thereby
- returning `TSKTrustDecisionShouldAllowConnection`.
+ For example, the pinning validation could have failed (ie. validationResult being 
+ `TSKTrustEvaluationFailedNoMatchingPin`) but the policy might be set to ignore pinning validation failures 
+ for this server, thereby returning `TSKTrustDecisionShouldAllowConnection`.
  */
 @property (nonatomic, readonly) TSKTrustDecision finalTrustDecision;
 
 /**
- The time in seconds it took for the SSL pinning validation to be performed.
+ The time it took for the SSL pinning validation to be performed.
  */
 @property (nonatomic, readonly) NSTimeInterval validationDuration;
 
 /**
- The certificate chain returned by the server as an array of PEM-formatted certificates.
+ The certificate chain extracted from the `serverTrust` as PEM-formatted certificates. This is the 
+ certificate chain sent by the server when establishing the connection.
  */
 @property (nonatomic, readonly, nullable) NSArray *certificateChain;
 
+/// :nodoc:
 - (instancetype _Nullable)initWithServerHostname:(NSString * _Nonnull)serverHostname
                            serverTrust:(SecTrustRef _Nonnull)serverTrust
                          notedHostname:(NSString * _Nonnull)notedHostname
