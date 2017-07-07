6ad3c9147ed2f179235f847816c2cf585d5d726c
Alban Diquet <ad@sourcetheorem.com>
2017-07-07 12:44:36 +0200

    Fix umbrella header

--- TrustKit/Framework/TrustKit-umbrella.h
+++ TrustKit/Framework/TrustKit-umbrella.h
@@ -1,17 +1,21 @@
-//
-//  TrustKit.h
-//  TrustKit
-//
-//  Created by Adam Kaplan on 6/8/17.
-//  Copyright © 2017 TrustKit. All rights reserved.
-//
+/*
+ 
+ TrustKit.h
+ TrustKit
+ 
+ Copyright 2017 The TrustKit Project Authors
+ Licensed under the MIT license, see associated LICENSE file for terms.
+ See AUTHORS file for the list of project authors.
+ 
+ */
 
 #ifndef _TRUSTKIT_
 #define _TRUSTKIT_
 
 #import <TrustKit/TSKTrustKitConfig.h>
-#import <TrustKit/TSKPublicKeyAlgorithm.h>
 #import <TrustKit/TSKPinningValidator.h>
-#import <TrustKit/TSKPinValidatorResult.h>
+#import <TrustKit/TSKPinningValidatorCallback.h>
+#import <TrustKit/TSKPinningValidatorResult.h>
+#import <TrustKit/TSKTrustDecision.h>
 
 #endif /* _TRUSTKIT_ */
