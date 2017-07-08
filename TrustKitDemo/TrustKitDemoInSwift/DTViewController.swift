3e9110565bf0b178b7e524eaf3dc70dd63ce34be
Adam Kaplan <adamkaplan@yahoo-inc.com>
2017-07-08 17:05:56 -0400

    Code scrub/simplification for demo app (Swift)

--- TrustKitDemo/TrustKitDemoInSwift/DTViewController.swift
+++ TrustKitDemo/TrustKitDemoInSwift/DTViewController.swift
@@ -14,63 +14,30 @@ import TrustKit
 
 class DTViewController: UIViewController, URLSessionDelegate {
 
-    @IBOutlet weak var testInvalidPinBtn: UIButton!
-    @IBOutlet weak var testValidPinBtn: UIButton!
-    var session: URLSession!
-    var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var testInvalidPinBtn: UIButton? {
+        didSet { testValidPinBtn?.layer.cornerRadius = 4; } // add rounded corners
+    }
+    
+    @IBOutlet weak var testValidPinBtn: UIButton? {
+        didSet { testValidPinBtn?.layer.cornerRadius = 4; }  // add rounded corners
+    }
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    lazy var session: URLSession = {
+        URLSession(configuration: URLSessionConfiguration.ephemeral,
+                                         delegate: self,
+                                         delegateQueue: OperationQueue.main)
+    }()
+    
     let baseURLYahoo = "https://www.yahoo.com/"
+    
     let baseURLDT = "https://www.datatheorem.com/"
     
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        
-        // Customize look and feel of buttons
-        self.testInvalidPinBtn.layer.cornerRadius = 4;
-        self.testValidPinBtn.layer.cornerRadius = 4;
-        
-        // Create URLSession object
-        self.session = URLSession(configuration: URLSessionConfiguration.ephemeral, delegate: self, delegateQueue: nil)
-        
-        // Create loader view
-        self.activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
-    }
-
-    func loadURL(url: URL)
-    {
-        // Show loading view
-        self.showActivityIndicatorInCurrentViewController()
-        
-        // Load a URL with a good pinning configuration
-        let task = self.session.dataTask(with: url) { (data, response, error) in
-            if error == nil {
-                // Display Success Alert
-                DispatchQueue.main.async {
-                    self.displayAlert(withTitle: "Test Result", messsage: "Pinning validation succeeded for \(url.absoluteString)")
-                }
-            }
-            else {
-                // Display Error Alert
-                DispatchQueue.main.async {
-                    self.displayAlert(withTitle: "Test Result", messsage: "Pinning validation failed for [\(url.absoluteString)] : [\(error.debugDescription)]")
-                }
-            }
-        }
-        task.resume()
-    }
+    // MARK: TrustKit Pinning Reference
     
-    @IBAction func testInvalidPinning(_ sender: UIButton)
-    {
-        self.loadURL(url: URL(string: baseURLYahoo)!)
-    }
-    
-    @IBAction func testValidPinning(_ sender: UIButton)
-    {
-        self.loadURL(url: URL(string: baseURLDT)!)
-    }
-    
-    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void)
-    {
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        
         // Call into TrustKit here to do pinning validation
         if TrustKit.sharedInstance().pinningValidator.handle(challenge, completionHandler: completionHandler) == false {
             // TrustKit did not handle this challenge: perhaps it was not for server trust
@@ -79,29 +46,54 @@ class DTViewController: UIViewController, URLSessionDelegate {
         }
     }
     
-    func displayAlert(withTitle title: String, messsage: String)
-    {
-        // Hide Activity Indicator
-        self.hideActivityIndicator()
+    // MARK: Test Control
+    
+    func loadURL(url: URL) {
+        // Show loading view
+        showActivityIndicatorInCurrentViewController()
         
-        let alertController = UIAlertController(title: title, message: messsage, preferredStyle: .alert)
+        // Load a URL with a good pinning configuration
+        let task = session.dataTask(with: url) { [weak self] (data, response, error) in
+            guard error == nil else {
+                // Display Error Alert
+                self?.displayAlert(withTitle: "Test Result",
+                                   message: "Pinning validation failed for \(url.absoluteString)\n\n\(error.debugDescription)")
+                return
+            }
+            
+            // Display Success Alert
+            self?.displayAlert(withTitle: "Test Result",
+                               message: "Pinning validation succeeded for \(url.absoluteString)")
+        }
+        
+        task.resume()
+    }
+    
+    @IBAction func testInvalidPinning(_ sender: UIButton) {
+        self.loadURL(url: URL(string: baseURLYahoo)!)
+    }
+    
+    @IBAction func testValidPinning(_ sender: UIButton) {
+        self.loadURL(url: URL(string: baseURLDT)!)
+    }
+    
+    func displayAlert(withTitle title: String, message: String) {
+        // Hide Activity Indicator
+        hideActivityIndicator()
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
     
-    func showActivityIndicatorInCurrentViewController()
-    {
-        self.view.isUserInteractionEnabled = false
-        self.activityIndicator.center = CGPoint(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height/2 + 100)
-        self.view.addSubview(self.activityIndicator)
-        self.activityIndicator.startAnimating()
+    func showActivityIndicatorInCurrentViewController() {
+        view.isUserInteractionEnabled = false
+        activityIndicator.startAnimating()
     }
     
-    func hideActivityIndicator()
-    {
-        self.view.isUserInteractionEnabled = true
-        self.activityIndicator.stopAnimating()
-        self.activityIndicator.removeFromSuperview()
+    func hideActivityIndicator() {
+        view.isUserInteractionEnabled = true
+        activityIndicator.stopAnimating()
     }
 }
 
