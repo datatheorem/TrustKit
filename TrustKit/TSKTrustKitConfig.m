c5e36cc00b3a0f147ed9887a0b7c51c965e0a6d8
Alban Diquet <ad@sourcetheorem.com>
2019-11-05 16:08:53 -0800

    Bump version number

--- TrustKit/TSKTrustKitConfig.m
+++ TrustKit/TSKTrustKitConfig.m
@@ -11,7 +11,7 @@
 
 #import "TSKTrustKitConfig.h"
 
-NSString * const TrustKitVersion = @"1.6.2";
+NSString * const TrustKitVersion = @"1.6.3";
 
 // General keys
 const TSKGlobalConfigurationKey kTSKSwizzleNetworkDelegates = @"TSKSwizzleNetworkDelegates";
